30b90e5254903c5fb8e7ccb8e0815e673ab2507c
İdris ADIGÜZEL <idrisadiguzel@gmail.com>
2022-06-25 17:40:42 +0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
    firebase: {
-    apiKey: "AIzaSyDJgmo7tpSuZQMqgpI-HrSqIOLMbas8c6E",
+    apiKey: "",
     authDomain: "idris-cv.firebaseapp.com",
     projectId: "idris-cv",
     storageBucket: "idris-cv.appspot.com",
-    messagingSenderId: "236751426135",
-    appId: "1:236751426135:web:18e9cee3b73313d0f40912",
-    measurementId: "G-KH2WSS3PP3"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
